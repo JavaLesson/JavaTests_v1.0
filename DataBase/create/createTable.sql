589de4880786b69771e0048b49527764949ce7af
akatomakhina <katomakhinaanastasiya@gmail.com>
2017-10-02 19:27:30 +0200

    new create SQL Script

--- DataBase/create/createTable.sql
+++ DataBase/create/createTable.sql
@@ -13,10 +13,10 @@ CREATE SCHEMA IF NOT EXISTS `JavaTests` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE IF NOT EXISTS `JavaTests`.`User` (
   `userId` INT(11) NOT NULL AUTO_INCREMENT,
-  `firstName` VARCHAR(45) NOT NULL,
-  `lastName` VARCHAR(45) NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `firstName` VARCHAR(100) NOT NULL,
+  `lastName` VARCHAR(100) NOT NULL,
+  `login` VARCHAR(100) NOT NULL,
+  `password` int(11) NOT NULL,
   PRIMARY KEY (`userId`),
   UNIQUE INDEX `userId_UNIQUE` (`userId` ASC),
   UNIQUE INDEX `login_UNIQUE` (`login` ASC),
@@ -26,15 +26,21 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `JavaTests`.`Question` (
   `questionId` INT(11) NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(1000) NOT NULL,
   `testId` INT(11) NOT NULL,
   `topicId` INT(11) NOT NULL,
   PRIMARY KEY (`questionId`, `testId`, `topicId`),
   UNIQUE INDEX `questionId_UNIQUE` (`questionId` ASC),
-  INDEX `fk_Question_test1_idx` (`testId` ASC, `topicId` ASC),
-  CONSTRAINT `fk_Question_test1`
-    FOREIGN KEY (`testId` , `topicId`)
-    REFERENCES `JavaTests`.`Test` (`testId` , `topicId`)
+  INDEX `idx_question_test` (`testId` ASC),
+  INDEX `idx_question_topic` (`topicId` ASC),
+  CONSTRAINT `fk_question_test`
+    FOREIGN KEY (`testId` )
+    REFERENCES `JavaTests`.`Test` (`testId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_question_topic`
+    FOREIGN KEY (`topicId`)
+    REFERENCES `JavaTests`.`Topic` (`topicId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -42,13 +48,13 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `JavaTests`.`Answer` (
   `answerId` INT(11) NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(1000) NOT NULL,
   `correct` TINYINT(1) NOT NULL,
   `questionId` INT(11) NOT NULL,
-  PRIMARY KEY (`answerId`, `questionId`),
+  PRIMARY KEY (`answerId`),
   UNIQUE INDEX `answerId_UNIQUE` (`answerId` ASC),
-  INDEX `fk_Answer_Question1_idx` (`questionId` ASC),
-  CONSTRAINT `fk_Answer_Question1`
+  INDEX `idx_answer_question` (`questionId` ASC),
+  CONSTRAINT `fk_answer`
     FOREIGN KEY (`questionId`)
     REFERENCES `JavaTests`.`Question` (`questionId`)
     ON DELETE NO ACTION
@@ -58,26 +64,26 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `JavaTests`.`Literature` (
   `literatureId` INT(11) NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(1000) NOT NULL,
   `questionId` INT(11) NOT NULL,
   `testId` INT(11) NOT NULL,
   `topicId` INT(11) NOT NULL,
-  PRIMARY KEY (`literatureId`, `questionId`, `testId`, `topicId`),
+  PRIMARY KEY (`literatureId`),
   UNIQUE INDEX `literatureId_UNIQUE` (`literatureId` ASC),
-  INDEX `fk_Literature_Question1_idx` (`questionId` ASC),
-  INDEX `fk_Literature_test1_idx` (`testId` ASC),
-  INDEX `fk_Literature_topic1_idx` (`topicId` ASC),
-  CONSTRAINT `fk_Literature_Question1`
+  INDEX `idx_literature_question` (`questionId` ASC),
+  INDEX `idx_literature_test` (`testId` ASC),
+  INDEX `idx_literature_topic` (`topicId` ASC),
+  CONSTRAINT `fk_literature_question`
     FOREIGN KEY (`questionId`)
     REFERENCES `JavaTests`.`Question` (`questionId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Literature_test1`
+  CONSTRAINT `fk_literature_test`
     FOREIGN KEY (`testId`)
     REFERENCES `JavaTests`.`Test` (`testId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Literature_topic1`
+  CONSTRAINT `fk_literature_topic`
     FOREIGN KEY (`topicId`)
     REFERENCES `JavaTests`.`Topic` (`topicId`)
     ON DELETE NO ACTION
@@ -87,17 +93,35 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `JavaTests`.`Link` (
   `linkId` INT(11) NOT NULL AUTO_INCREMENT,
-  `link` VARCHAR(45) NULL DEFAULT NULL,
+  `link` VARCHAR(100) NULL DEFAULT NULL,
   `literatureId` INT(11) NOT NULL,
   `questionId` INT(11) NOT NULL,
   `testId` INT(11) NOT NULL,
   `topicId` INT(11) NOT NULL,
-  PRIMARY KEY (`linkId`, `literatureId`, `questionId`, `testId`, `topicId`),
+  PRIMARY KEY (`linkId`),
   UNIQUE INDEX `linksId_UNIQUE` (`linkId` ASC),
-  INDEX `fk_Link_Literature1_idx` (`literatureId` ASC, `questionId` ASC, `testId` ASC, `topicId` ASC),
-  CONSTRAINT `fk_Link_Literature1`
-    FOREIGN KEY (`literatureId` , `questionId` , `testId` , `topicId`)
-    REFERENCES `JavaTests`.`Literature` (`literatureId` , `questionId` , `testId` , `topicId`)
+  INDEX `idx_link_literature` (`literatureId` ASC),
+  INDEX `idx_link_question` (`questionId` ASC),
+  INDEX `idx_link_test` (`testId` ASC),
+  INDEX `idx_link_topic` (`topicId` ASC),
+  CONSTRAINT `fk_link_literature`
+    FOREIGN KEY (`literatureId`)
+    REFERENCES `JavaTests`.`Literature` (`literatureId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_link_question`
+    FOREIGN KEY (`questionId`)
+    REFERENCES `JavaTests`.`Question` (`questionId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_link_test`
+    FOREIGN KEY (`testId`)
+    REFERENCES `JavaTests`.`Test` (`testId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_link_topic`
+    FOREIGN KEY (`topicId`)
+    REFERENCES `JavaTests`.`Topic` (`topicId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -105,7 +129,7 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `JavaTests`.`Topic` (
   `topicId` INT(11) NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(1000) NOT NULL,
   PRIMARY KEY (`topicId`),
   UNIQUE INDEX `topicId_UNIQUE` (`topicId` ASC))
 ENGINE = InnoDB
@@ -117,8 +141,8 @@ CREATE TABLE IF NOT EXISTS `JavaTests`.`Test` (
   `topicId` INT(11) NOT NULL,
   PRIMARY KEY (`testId`, `topicId`),
   UNIQUE INDEX `testId_UNIQUE` (`testId` ASC),
-  INDEX `fk_test_topic1_idx` (`topicId` ASC),
-  CONSTRAINT `fk_test_topic1`
+  INDEX `idx_test_topic` (`topicId` ASC),
+  CONSTRAINT `fk_test`
     FOREIGN KEY (`topicId`)
     REFERENCES `JavaTests`.`Topic` (`topicId`)
     ON DELETE NO ACTION
@@ -129,24 +153,54 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `JavaTests`.`Statistic` (
   `statisticId` INT(11) NOT NULL AUTO_INCREMENT,
   `correct` TINYINT(1) NOT NULL,
+  `date` DATE not null,
   `userId` INT(11) NOT NULL,
   `questionId` INT(11) NOT NULL,
   `testId` INT(11) NOT NULL,
   `topicId` INT(11) NOT NULL,
-  PRIMARY KEY (`statisticId`, `userId`, `questionId`, `testId`, `topicId`),
+  PRIMARY KEY (`statisticId`),
   UNIQUE INDEX `userQuestionStatisticId_UNIQUE` (`statisticId` ASC),
-  INDEX `fk_UserQuestionStatistic_User_idx` (`userId` ASC),
-  INDEX `fk_UserQuestionStatistic_Question1_idx` (`questionId` ASC, `testId` ASC, `topicId` ASC),
-  CONSTRAINT `fk_UserQuestionStatistic_User`
+  INDEX `idx_userQuestionStatistic_User` (`userId` ASC),
+  INDEX `idx_userQuestionStatistic_question` (`questionId` ASC),
+  INDEX `idx_userQuestionStatistic_test` (`testId` ASC),
+  INDEX `idx_userQuestionStatistic_topic` (`topicId` ASC),
+  CONSTRAINT `fk_userQuestionStatistic_user`
     FOREIGN KEY (`userId`)
     REFERENCES `JavaTests`.`User` (`userId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_UserQuestionStatistic_Question1`
-    FOREIGN KEY (`questionId` , `testId` , `topicId`)
-    REFERENCES `JavaTests`.`Question` (`questionId` , `testId` , `topicId`)
+  CONSTRAINT `fk_userQuestionStatistic_question`
+    FOREIGN KEY (`questionId`)
+    REFERENCES `JavaTests`.`Question` (`questionId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_userQuestionStatistic_test`
+    FOREIGN KEY (`testId`)
+    REFERENCES `JavaTest`.`Test` (`testId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_userQuestionStatistic_topic`
+    FOREIGN KEY (`topicId`)
+    REFERENCES `JavaTest`.`Topic` (`topicId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+create table if not exists `JavaTests`.`TaskTracker`(
+	`taskTrackerId` INT(11) NOT NULL AUTO_INCREMENT,
+    `startDate` Date not null,
+    `endDate` Date NOT NULL,
+    `compleat` TINYINT(1) not null,
+    `userId` int(11) not null,
+    primary key (`taskTrackerId`),
+    index `idx_taskTracker_user`(`userId` asc),
+    constraint `fk_taskTracker_user` 
+    FOREIGN KEY (`userId`)
+    REFERENCES `JavaTests`.`User` (`userId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION    
+) 
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
